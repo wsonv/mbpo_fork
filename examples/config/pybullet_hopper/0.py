c9d2bc7d5b477be5807c8ade1c2f7b97286efbdd
wonjun <wonjunson@Workstation>
2020-05-17 16:13:27 -0400

    fork

---
+++ examples/config/pybullet_hopper/0.py
@@ -0,0 +1,35 @@
+params = {
+    'type': 'MBPO',
+    'universe': 'gym',
+    'domain': 'PybulletHopper',
+    'task': 'v0',
+
+    'log_dir': '~/ray_mbpo/',
+    'exp_name': 'defaults',
+
+    'kwargs': {
+        'n_epochs': 5000,
+        'epoch_length': 100,
+        'train_every_n_steps': 1,
+        'n_train_repeat': 20,
+        'eval_render_mode': None,
+        'eval_n_episodes': 1,
+        'eval_deterministic': True,
+
+        'discount': 0.99,
+        'tau': 5e-3,
+        'reward_scale': 1.0,
+
+        'model_train_freq': 250,
+        'model_retain_epochs': 1,
+        'rollout_batch_size': 100e3,
+        'deterministic': False,
+        'num_networks': 7,
+        'num_elites': 5,
+        'real_ratio': 0.05,
+        'target_entropy': -1,
+        'max_model_t': None,
+        'rollout_schedule': [20, 150, 1, 15],
+    }
+}
+
